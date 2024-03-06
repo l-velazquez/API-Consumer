fab4d6d36d950ce2f544edfcec8a470881e0b79a
Luis <luisfernandojavier.velazquez@upr.edu>
2024-03-05 20:58:11 -0400

    Fix bug in login functionality

--- API_consumer.py
+++ API_consumer.py
@@ -55,15 +55,15 @@ def DELETE_request(url, headers):
     return response_data(requests.delete(url, headers=headers))
 
 
-def username_creator(full_name):
+def username_creator(full_name, id):
     full_name = full_name.split()
-    username = full_name[0][0].lower() + full_name[1].lower()
+    username = full_name[0][0].lower() + full_name[1].lower() + str(id)
     return username
 
 
 def email_creator(full_name):
     full_name = full_name.split()
-    email = full_name[0].lower() + full_name[1].lower() + "@test.com"
+    email = full_name[0].lower() + full_name[1].lower() + "2026" + "@test.com"
     return email
 
 
@@ -73,7 +73,8 @@ def password_creator():
     return password
 
 
-def get_users(range, base_url, admin_login, headers):
+# GET
+def get_users_range(range, base_url, admin_login, headers):
     headers = headers  # make a copy of the headers
     # Endpoints
     authenticate = ["/Auth/login", "/Auth/logout"]
@@ -110,7 +111,75 @@ def get_users(range, base_url, admin_login, headers):
     print("-" * 140, f"\nTotal amount of users: {amount_of_users}")
 
 
-def get_products(range, base_url, admin_login, headers):
+def get_users(base_url, admin_login, headers):
+    headers = headers  # make a copy of the headers
+    # Endpoints
+    authenticate = ["/Auth/login", "/Auth/logout"]
+    users = "/Users"
+
+    url_auth = base_url + authenticate[0]
+    url_users = base_url + users
+
+    # login as admin
+    print("Logging in as admin")
+    print("-" * 140, "\n")
+
+    admin_response = POST_request(url_auth, admin_login, headers)
+    if not (admin_response["success"]):
+        print("No data returned")
+        # get out of the function
+        return
+
+    token = admin_response["data"]["accessToken"]  # get token from response
+    headers["Authorization"] = "Bearer " + token  # add token to headers
+
+    users = GET_request(url_users, headers)
+    print("Users:", users)
+    # display all users full names
+    for user in users["data"]:
+        print("\t" + user["fullName"])
+
+
+def user_pagination(base_url, admin_login, headers):
+    headers = headers  # make a copy of the headers
+    # Endpoints
+    authenticate = ["/Auth/login", "/Auth/logout"]
+    users = "/Users"
+
+    url_auth = base_url + authenticate[0]
+    url_users = base_url + users
+
+    # login as admin
+    print("Logging in as admin")
+    print("-" * 140, "\n")
+
+    admin_response = POST_request(url_auth, admin_login, headers)
+    if not (admin_response["success"]):
+        print("No data returned")
+        # get out of the function
+        return
+
+    token = admin_response["data"]["accessToken"]  # get token from response
+    headers["Authorization"] = "Bearer " + token  # add token to headers
+
+    page_number = input("Enter the page number: ")
+    page_size = input("Enter the page size: ")
+
+    url_users_paginated = (
+        url_users + f"/paginated?pageNumber={page_number}&pageSize={page_size}"
+    )
+
+    users = GET_request(url_users_paginated, headers)
+    # save_json(users, "users_paginated.json")
+    # save_json(users, "users_2.json")
+
+    users_all = []
+    for user in users["data"]["data"]:
+        users_all.append(user)
+        print("\t" + user["fullName"])
+
+
+def get_products(base_url, admin_login, headers):
     headers = headers  # make a copy of the headers
     # Endpoints
     authenticate = ["/Auth/login", "/Auth/logout"]
@@ -132,30 +201,13 @@ def get_products(range, base_url, admin_login, headers):
     token = admin_response["data"]["accessToken"]  # get token from response
     headers["Authorization"] = "Bearer " + token  # add token to headers
 
-    amount_of_products = 0
-
     products = GET_request(url_products, headers)
-    # save_json(products, "products.json")
+    save_json(products, "products_2.json")
 
-    for i in range:
-        product = GET_request(url_products + "/" + str(i), headers)
-
-        if not (product["success"]):
-            print("\tNo data returned")
-            print("-" * 140, f"\nTotal amount of products: {amount_of_products}")
-            return
-            # get out of the function
-        else:
-            print("\tProduct " + str(i))
-            print("-" * 140, "\n")
-            print("\tID:\t\t" + str(product["data"]["id"]))
-            print("\tName:\t\t" + product["data"]["name"])
-            print("\tDescription:\t" + product["data"]["description"])
-            print("\tPrice:\t\t$" + str(product["data"]["price"]))
-            print("\tQuantity:\t" + str(product["data"]["quantity"]))
-            print("\n")
-            amount_of_products += 1
-    print("-" * 140, f"\nTotal amount of products: {amount_of_products}")
+    products_all = []
+    for product in products["data"]:
+        products_all.append(product)
+        print("\t" + product["name"])
 
 
 def get_roles(range, base_url, admin_login, headers):
@@ -180,16 +232,20 @@ def get_roles(range, base_url, admin_login, headers):
     token = admin_response["data"]["accessToken"]  # get token from response
     headers["Authorization"] = "Bearer " + token  # add token to headers
 
-    print(GET_request(url_roles, headers))
+    roles = GET_request(url_roles, headers)
+
+    roles_id = []
+    for role in roles["data"]:
+        roles_id.append(role["id"])
 
     amount_of_roles = 0
 
-    for i in range:
+    for i in roles_id:
         role = GET_request(url_roles + "/" + str(i), headers)
 
         if not (role["success"]):
             print("\tNo data returned")
-            return
+            continue
             # get out of the function
         else:
             print("\tRole " + str(i))
@@ -199,6 +255,7 @@ def get_roles(range, base_url, admin_login, headers):
     print("-" * 140, f"\nTotal amount of roles: {amount_of_roles}")
 
 
+# POST
 def post_users(base_url, login, headers, full_name_array):
     headers = headers  # make a copy of the headers
     # Endpoints
@@ -221,7 +278,7 @@ def post_users(base_url, login, headers, full_name_array):
 
     user_name_array = []
     for name in full_name_array:
-        user_name_array.append(username_creator(name))
+        user_name_array.append(username_creator(name, 1020))
 
     email_array = []
     for name in full_name_array:
@@ -247,7 +304,78 @@ def post_users(base_url, login, headers, full_name_array):
     print(f"Logged out as {login['userName']}")
 
 
+def post_products(base_url, login, headers):
+    headers = headers  # make a copy of the headers
+    # Endpoints
+    authenticate = ["/Auth/login", "/Auth/logout"]
+    products = "/Products"
+
+    url_auth = base_url + authenticate[0]
+    url_logout = base_url + authenticate[1]
+    url_products = base_url + products
+
+    # login as admin
+    print(f"Logging in as {login['userName']}")
+    admin_response = POST_request(url_auth, login, headers)
+    token = admin_response["data"]["accessToken"]  # get token from response
+    headers["Authorization"] = "Bearer " + token  # add token to headers
+
+    # Post data
+    print("\n\n\n\nPOSTing data")
+
+    name = input("Enter the product name: ")
+    price = input("Enter the product price: ")
+    description = input("Enter the product description: ")
+
+    product_data = {"name": name, "price": price, "description": description}
+
+    product = POST_request(url_products, product_data, headers)
+    print("\n", product)
+
+    # logout as admin
+    POST_request(url_logout, {}, headers)
+    print(f"Logged out as {login['userName']}")
+
+
+def post_roles(base_url, login, headers):
+    headers = headers  # make a copy of the headers
+    # Endpoints
+    authenticate = ["/Auth/login", "/Auth/logout"]
+    roles = "/Roles"
+
+    url_auth = base_url + authenticate[0]
+    url_logout = base_url + authenticate[1]
+    url_roles = base_url + roles
+
+    # login as admin
+    print(f"Logging in as {login['userName']}")
+    admin_response = POST_request(url_auth, login, headers)
+    token = admin_response["data"]["accessToken"]  # get token from response
+    headers["Authorization"] = "Bearer " + token  # add token to headers
+
+    # Post data
+    print("\n\n\n\nPOSTing data")
+
+    code = input("Enter the role code: ")
+    name = input("Enter the role name: ")
+    is_active = input("Is the role active? (y/n): ")
+    if is_active == "y":
+        is_active = True
+    else:
+        is_active = False
+
+    role_data = {"code": code, "name": name, "isActive": is_active}
+
+    role = POST_request(url_roles, role_data, headers)
+    print("\n", role)
+
+    # logout as admin
+    POST_request(url_logout, {}, headers)
+    print(f"Logged out as {login['userName']}")
+
+
 def put_users(base_url, login, headers):
+
     headers = headers  # make a copy of the headers
     # Endpoints
     authenticate = ["/Auth/login", "/Auth/logout"]
@@ -327,6 +455,145 @@ def put_users(base_url, login, headers):
     # logout as admin
 
 
+# Delete
+def delete_user(base_url, login, headers):
+    headers = headers  # make a copy of the headers
+    # Endpoints
+    authenticate = ["/Auth/login", "/Auth/logout"]
+    users = "/Users"
+
+    url_auth = base_url + authenticate[0]
+    url_logout = base_url + authenticate[1]
+    url_users = base_url + users
+
+    # login as admin
+    print(f"Logging in as {login['userName']}")
+    admin_response = POST_request(url_auth, login, headers)
+    token = admin_response["data"]["accessToken"]  # get token from response
+    headers["Authorization"] = "Bearer " + token  # add token to headers
+
+    # Post data
+    print("\n\n\n\nDELETEing data")
+
+    method = "DELETE"
+
+    # get all users in the system
+    users_data = GET_request(url_users, headers)
+    # list of all users names
+    user_names = []
+    for user in users_data["data"]:
+        user_names.append(user["userName"])
+    count = 0
+    for user in user_names:
+        count += 1
+        print(str(count) + ".\t", user)
+
+    # Chose user to update
+    user_id = input("\nEnter the number of the user you want to delete: ")
+
+    # Get user data with the id
+    user_data = GET_request(url_users + "/" + user_id, headers)
+    data_of_user = user_data["data"]
+    full_name = data_of_user["fullName"]
+    user_name = data_of_user["userName"]
+    email = data_of_user["email"]
+    role = data_of_user["role"]
+
+    print("\n\tUser data: ")
+    print("\tID: ", user_id)
+    print("\tFull Name: ", full_name)
+    print("\tUsername: ", user_name)
+    print("\tEmail: ", email)
+    print("\tRole: ", role)
+    print("\n")
+
+    ays = input(f"Are you sure you want to delete {full_name}? (y/n): ")
+    if ays == "n":
+        print("Exiting")
+        delete_user(base_url, login, headers)
+    delete_user = DELETE_request(url_users + "/" + user_id, headers)
+
+    success = delete_user["success"]
+    message = delete_user["message"]
+
+    if success:
+        print(message)
+
+    agian = input("Do you want to delete another user? (y/n): ")
+    if agian == "y":
+        delete_user(base_url, login, headers)
+    else:
+        print("Logging out")
+        POST_request(url_logout, {}, headers)
+        print(f"Logged out as {login['userName']}")
+
+
+def delete_role(base_url, login, headers):
+    headers = headers  # make a copy of the headers
+    # Endpoints
+    authenticate = ["/Auth/login", "/Auth/logout"]
+    roles = "/Roles"
+
+    url_auth = base_url + authenticate[0]
+    url_logout = base_url + authenticate[1]
+    url_roles = base_url + roles
+
+    # login as admin
+    print(f"Logging in as {login['userName']}")
+    admin_response = POST_request(url_auth, login, headers)
+    token = admin_response["data"]["accessToken"]  # get token from response
+    headers["Authorization"] = "Bearer " + token  # add token to headers
+
+    # Post data
+    print("\n\n\n\nDELETEing data")
+
+    method = "DELETE"
+
+    # get all users in the system
+    roles_data = GET_request(url_roles, headers)
+
+    # list of all users names
+    role_names = []
+    for role in roles_data["data"]:
+        role_names.append(str(role["id"]) + ":\t" + role["name"])
+
+    for role in role_names:
+        print(role)
+
+    # Chose user to update
+    role_id = input("\nEnter the number of the role you want to delete: ")
+
+    # Get user data with the id
+    role_data = GET_request(url_roles + "/" + role_id, headers)
+    data_of_role = role_data["data"]
+    role_name = data_of_role["name"]
+
+    print("\n\tRole data: ")
+    print("\tID: ", role_id)
+    print("\tRole Name: ", role_name)
+    print("\n")
+
+    ays = input(f"Are you sure you want to delete {role_name}? (y/n): ")
+    if ays == "n":
+        print("Exiting")
+        delete_role(base_url, login, headers)
+    delete_role = DELETE_request(url_roles + "/" + role_id, headers)
+    print("\n", delete_role)
+    success = delete_role["success"]
+    message = delete_role["message"]
+    if success:
+        print(message)
+
+    agian = input("Do you want to delete another role? (y/n): ")
+    if agian == "y":
+        delete_role(base_url, login, headers)
+    else:
+        print("Logging out")
+        POST_request(url_logout, {}, headers)
+        print(f"Logged out as {login['userName']}")
+
+
+# Admin requests
 def admin_requests(base_url, admin_login, headers):
     headers = headers  # make a copy of the headers
     # Endpoints
@@ -404,6 +671,7 @@ def admin_requests(base_url, admin_login, headers):
     print("Logged out as admin")
 
 
+# User requests
 def user_requests(base_url, user_login, headers):
     headers = headers  # make a copy of the headers
     # Endpoints
@@ -460,16 +728,16 @@ def main():
     # admin_requests(base_url, admin_login, headers)
     # user_requests(base_url, user_login, headers)
 
-    full_name_array = [
-        "Eliam Rodriguez",
-    ]
-
     # get_users(range(1, 36), base_url, admin_login, headers)
-    # get_products(range(1, 200), base_url, admin_login, headers)
-    # get_users(range(1, 36), base_url, user_login, headers)
+    # get_products(base_url, admin_login, headers)
+    # get_users(base_url, user_login, headers)
     # post_users(base_url, user_login, headers, full_name_array)
-    # get_roles(range(1, 36), base_url, admin_login, headers)
-    put_users(base_url, admin_login, headers)
+    # get_roles(range(1, 20), base_url, admin_login, headers)
+    # put_users(base_url, admin_login, headers)
+    # delete_user(base_url, user_login, headers)
+    # delete_role(base_url, admin_login, headers)
+    # post_roles(base_url, admin_login, headers)
+    user_pagination(base_url, admin_login, headers)
 
 
 if __name__ == "__main__":
